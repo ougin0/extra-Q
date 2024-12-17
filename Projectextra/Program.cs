b7589d18abe4ee4062ba98dfbd68b78543a4ed5c
fasolksa <ftyy2015@hotmail.com>
2024-12-17 17:11:33 +0300

    1

---
+++ Projectextra/Program.cs
@@ -0,0 +1,50 @@
+﻿using System; 
+
+/*
+//Q1
+string name1 = "Faisal";
+Console.WriteLine(name1);
+  */
+ 
+/*
+//Q2
+Console.WriteLine("enter two number");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine(number1+number2);
+*/
+/*
+//Q4
+Console.WriteLine("enter two number");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
+if (number1>number2){
+    Console.WriteLine(number1 + " is larger");
+}
+else {
+    Console.WriteLine(number2 + " is larger");
+}
+*/
+/*
+//Q5
+Console.WriteLine("enter one of (A, B, C, D, F)");
+string AS = Console.ReadLine();
+switch(AS){
+    case "A" :
+    Console.WriteLine("Excellent");
+    break;
+    case "B" :
+    Console.WriteLine("Very Good");
+    break;
+    case "C" :
+    Console.WriteLine("Good");
+    break;
+    case "D" :
+    Console.WriteLine("Needs Improvement");
+    break;
+    case "F" :
+    Console.WriteLine("Fail");
+    break;
+
+}
+*/
